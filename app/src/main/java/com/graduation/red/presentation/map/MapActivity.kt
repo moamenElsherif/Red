807cdfa7faec4c45f8cb5e707b5dcb512f63fb3a
moamen <moamen.ahmed@shgardi.app>
2023-05-04 16:14:05 +0300

    maps start

---
+++ app/src/main/java/com/graduation/red/presentation/map/MapActivity.kt
@@ -0,0 +1,16 @@
+package com.graduation.red.presentation.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.graduation.red.R
+import com.graduation.red.base.BaseActivity
+import com.graduation.red.databinding.ActivityMapBinding
+
+class MapActivity : BaseActivity<ActivityMapBinding>()  {
+    override val layoutRes: Int
+        get() = R.layout.activity_map
+
+    override fun initUI(savedInstanceState: Bundle?) {
+
+    }
+}
